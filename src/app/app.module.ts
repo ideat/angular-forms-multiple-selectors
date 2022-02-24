39f7cc980751eefe8965848406baa07d1d7af807
freddy <jfreddy.caballero@gmail.com>
2022-02-24 15:45:41 -0400

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { PaisesModule } from './paises/paises.module';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    PaisesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/paises/pages/selector-page/selector-page.component.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { switchMap, tap } from 'rxjs/operators';
+import { PaisSmall } from '../../interfaces/paises.interfaces';
+import { PaisesService } from '../../services/paises.service';
+
+@Component({
+  selector: 'app-selector-page',
+  templateUrl: './selector-page.component.html',
+  styles: [
+  ]
+})
+export class SelectorPageComponent implements OnInit {
+
+  miFormulario: FormGroup = this.fb.group({
+    region: ['', Validators.required],
+    pais: ['', Validators.required],
+    frontera: ['', Validators.required]
+
+  });
+
+  regiones: string[] = [];
+  paises: PaisSmall[]=[];
+  // fronteras: string[] = [];
+  fronteras: PaisSmall[] = [];
+
+  cargando: boolean = false;
+
+  constructor( private fb: FormBuilder,
+               private paisesService: PaisesService) { }
+
+  ngOnInit() {
+
+    this.regiones = this.paisesService.regiones;
+
+    //Cuando cambie la region
+    this.miFormulario.get('region')?.valueChanges
+      .pipe(
+        tap( _ => {
+          this.miFormulario.get('pais')?.reset('');
+          this.cargando = true;
+        } ),
+        switchMap( region => this.paisesService.getPaisesPorRegion( region ))
+      )
+      .subscribe(paises => {
+        this.paises = paises;
+        this.cargando = false;
+      });
+
+      //Cuando cambia el pais
+      this.miFormulario.get('pais')?.valueChanges
+        .pipe(
+          tap( () => {
+            this.fronteras = [];
+            this.miFormulario.get('frontera')?.reset('');
+            this.cargando=true
+          }),
+          switchMap( codigo => this.paisesService.getPaisPorCodigo(codigo)),
+          switchMap ( pais =>this.paisesService.getPaisesPorCodigo(pais?.borders!) )
+        )
+        .subscribe( paises => {
+          // this.fronteras = pais?.borders || [];
+          this.fronteras = paises;
+          this.cargando=false;
+        })
+
+  }
+
+  guardar(){
+    console.log(this.miFormulario.value);
+  }
+}
